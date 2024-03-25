bd7e566379348000194253f5a89a1eefc597e6a7
Zhong, Zhicong <zhicong.zhong@intel.com>
2024-03-25 01:46:24 -0700

    generalize the dedfault config

--- lib/TPP/Transforms/RewriteMatmulToNestedMatmul.cpp
+++ lib/TPP/Transforms/RewriteMatmulToNestedMatmul.cpp
@@ -39,9 +39,35 @@ struct MatmulConfig {
   int innerMostMBlock, innerMostNBlock, innerMostKBlock;
 };
 
-[[maybe_unused]] MatmulConfig
-getMatmulConfig(const linalg::MatmulOp &matmulOp) {
-  return {32, 32, 32, 4, 14, 1, 8, 8, 8};
+template <typename T> inline T divAndCeil(T a, T b) { return (a - 1) / b + 1; }
+
+MatmulConfig getDefaultMatmulConfig(linalg::MatmulOp &linalgOp) {
+  auto M = linalgOp.getShape(linalgOp.getDpsInputOperand(0))[0],
+       N = linalgOp.getShape(linalgOp.getDpsInputOperand(1))[1],
+       K = linalgOp.getShape(linalgOp.getDpsInputOperand(1))[0];
+  MatmulConfig cfg;
+
+  // innermost Block
+  auto defaultBlock = 32;
+  cfg.innerMostMBlock = M % defaultBlock == 0 ? defaultBlock : M;
+  cfg.innerMostNBlock = N % defaultBlock == 0 ? defaultBlock : N;
+  cfg.innerMostKBlock = K % defaultBlock == 0 ? defaultBlock : K;
+
+  // Number of block
+  auto MNumBlock = M / cfg.innerMostMBlock;
+  auto NNumBlock = N / cfg.innerMostNBlock;
+  auto KNumBlock = K / cfg.innerMostKBlock;
+
+  // Threads
+  cfg.MThreads = 1;
+  cfg.NThreads = 1;
+  cfg.KThreads = 1;
+
+  // Block
+  cfg.MBlock = divAndCeil((int)MNumBlock, cfg.MThreads);
+  cfg.NBlock = divAndCeil((int)NNumBlock, cfg.NThreads);
+  cfg.KBlock = divAndCeil((int)KNumBlock, cfg.KThreads);
+  return cfg;
 }
 
 /*
@@ -108,214 +134,148 @@ struct RewriteMatmulToNestedMatmul
       (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
     }
 
-    // Pack matmul, will be replaced by blocked tensor dialect pass
-    auto MBlock = 32, NBlock = 32, KBlock = 32;
-    auto MThreads = 3, NThreads = 3, KThreads = 1;
-    auto innerMostMBlock = 8, innerMostNBlock = 8, innerMostKBlock = 8;
-
-    // Step 2. Pack to Mkmk(innerMostMBlock, innerMostKBlock) amd
-    // NKkn(inermostKBlock, innermostNBlock)
     getOperation()->walk([&](linalg::MatmulOp matmulOp) {
       if (matmulOp.hasPureBufferSemantics())
         return signalPassFailure();
-      SmallVector<OpFoldResult> packSizes(
-          matmulOp.getNumLoops(),
-          getAsIndexOpFoldResult(rewriter.getContext(), 0));
-      packSizes[0] =
-          getAsIndexOpFoldResult(rewriter.getContext(), innerMostMBlock);
-      packSizes[1] =
-          getAsIndexOpFoldResult(rewriter.getContext(), innerMostNBlock);
-      packSizes[2] =
-          getAsIndexOpFoldResult(rewriter.getContext(), innerMostKBlock);
 
-      OpBuilder::InsertionGuard guard(rewriter);
-      rewriter.setInsertionPoint(matmulOp);
-      [[maybe_unused]] auto res =
-          mlir::linalgx::packMatmulOp(rewriter, matmulOp, packSizes);
-    });
+      MatmulConfig cfg = getDefaultMatmulConfig(matmulOp);
 
-    // Step 3. The processes of transforming matmul to nested matmul
-    getOperation()->walk([&](linalg::GenericOp linalgOp) {
-      // // TODO: check whether the op is matmul or packed matmul op
-      // auto M = linalgOp.getShape(linalgOp.getDpsInputOperand(0))[0],
-      //      N = linalgOp.getShape(linalgOp.getDpsInputOperand(1))[1],
-      //      K = linalgOp.getShape(linalgOp.getDpsInputOperand(1))[0];
-      if (linalgOp.hasPureBufferSemantics())
-        return signalPassFailure();
-      auto matmulOp = linalgOp;
+      // Step 2. Pack to Mkmk(innerMostMBlock, innerMostKBlock) amd
+      // NKkn(inermostKBlock, innermostNBlock)
+      linalg::LinalgOp genericOp;
+      {
+        SmallVector<OpFoldResult> packSizes(
+            matmulOp.getNumLoops(),
+            getAsIndexOpFoldResult(rewriter.getContext(), 0));
+        packSizes[0] =
+            getAsIndexOpFoldResult(rewriter.getContext(), cfg.innerMostMBlock);
+        packSizes[1] =
+            getAsIndexOpFoldResult(rewriter.getContext(), cfg.innerMostNBlock);
+        packSizes[2] =
+            getAsIndexOpFoldResult(rewriter.getContext(), cfg.innerMostKBlock);
+
+        OpBuilder::InsertionGuard guard(rewriter);
+        rewriter.setInsertionPoint(matmulOp);
+
+        auto linalgOp =
+            mlir::linalgx::packMatmulOp(rewriter, matmulOp, packSizes);
+
+        if (failed(linalgOp))
+          return signalPassFailure();
+
+        if (linalgOp->hasPureBufferSemantics())
+          return signalPassFailure();
+        genericOp = *linalgOp;
+      }
+
+      // Step 3. The processes of transforming matmul to nested matmul
       // 3.1 Parallel Loop with scf::forall
       {
         SmallVector<OpFoldResult> numThreads(
-            matmulOp.getNumLoops(),
+            genericOp.getNumLoops(),
             getAsIndexOpFoldResult(rewriter.getContext(), 0));
-        numThreads[0] = getAsIndexOpFoldResult(rewriter.getContext(), MThreads);
-        numThreads[1] = getAsIndexOpFoldResult(rewriter.getContext(), NThreads);
+        numThreads[0] =
+            getAsIndexOpFoldResult(rewriter.getContext(), cfg.MThreads);
+        numThreads[1] =
+            getAsIndexOpFoldResult(rewriter.getContext(), cfg.NThreads);
         numThreads[2] = getAsIndexOpFoldResult(rewriter.getContext(), 0);
         OpBuilder::InsertionGuard guard(rewriter);
-        rewriter.setInsertionPoint(matmulOp);
+        rewriter.setInsertionPoint(genericOp);
         auto tilingResult = linalg::tileToForallOp(
-            rewriter, cast<TilingInterface>(matmulOp.getOperation()),
+            rewriter, cast<TilingInterface>(genericOp.getOperation()),
             numThreads,
             /*mapping=*/std::nullopt);
         if (failed(tilingResult))
           return signalPassFailure();
-        rewriter.replaceOp(matmulOp, tilingResult->tileOp->getResults());
-        matmulOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOp);
+        rewriter.replaceOp(genericOp, tilingResult->tileOp->getResults());
+        genericOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOp);
       }
 
       // 3.2 Tiling reduction parallel loop with scf::forall
       // TODO: move the reduction dim to the front. (M, N, threads) ->
       // (threads, M, N)
       {
-        if (KThreads > 1) {
-          if (matmulOp.hasPureBufferSemantics())
+        if (cfg.KThreads > 1) {
+          if (genericOp.hasPureBufferSemantics())
             return signalPassFailure();
           SmallVector<OpFoldResult> tileSize(
-              matmulOp.getNumLoops(),
+              genericOp.getNumLoops(),
               getAsIndexOpFoldResult(rewriter.getContext(), 0));
-          tileSize[2] = getAsIndexOpFoldResult(rewriter.getContext(), KThreads);
+          tileSize[2] =
+              getAsIndexOpFoldResult(rewriter.getContext(), cfg.KThreads);
           tileSize[5] = getAsIndexOpFoldResult(rewriter.getContext(), 1);
 
-          rewriter.setInsertionPoint(matmulOp);
+          rewriter.setInsertionPoint(genericOp);
           auto tilingResult = scf::tileReductionUsingScf(
               rewriter,
-              cast<PartialReductionOpInterface>(matmulOp.getOperation()),
+              cast<PartialReductionOpInterface>(genericOp.getOperation()),
               tileSize);
           if (failed(tilingResult))
             return signalPassFailure();
-          matmulOp = dyn_cast<linalg::GenericOp>(tilingResult->parallelTiledOp);
+          genericOp =
+              dyn_cast<linalg::GenericOp>(tilingResult->parallelTiledOp);
         }
       }
 
       // 3.3 Tiling outer loop with scf::for
       {
-        if (matmulOp.hasPureBufferSemantics())
+        if (genericOp.hasPureBufferSemantics())
           return signalPassFailure();
         scf::SCFTilingOptions tileOption;
         SmallVector<OpFoldResult> TileSizes(
-            matmulOp.getNumLoops(),
+            genericOp.getNumLoops(),
             getAsIndexOpFoldResult(rewriter.getContext(), 0));
         TileSizes[0] = getAsIndexOpFoldResult(
-            rewriter.getContext(), (MBlock - 1) / innerMostMBlock + 1);
+            rewriter.getContext(), (cfg.MBlock - 1) / cfg.innerMostMBlock + 1);
         TileSizes[1] = getAsIndexOpFoldResult(
-            rewriter.getContext(), (NBlock - 1) / innerMostNBlock + 1);
+            rewriter.getContext(), (cfg.NBlock - 1) / cfg.innerMostNBlock + 1);
         TileSizes[2] = getAsIndexOpFoldResult(
-            rewriter.getContext(), (KBlock - 1) / innerMostKBlock + 1);
+            rewriter.getContext(), (cfg.KBlock - 1) / cfg.innerMostKBlock + 1);
         tileOption.setTileSizes(TileSizes);
 
-        SmallVector<int64_t> interchange(matmulOp.getNumLoops(), 0);
-        for (auto i = 0UL; i < matmulOp.getNumLoops(); i++) {
+        SmallVector<int64_t> interchange(genericOp.getNumLoops(), 0);
+        for (auto i = 0UL; i < genericOp.getNumLoops(); i++) {
           interchange[i] = i;
         }
         tileOption.setInterchange(interchange);
 
         OpBuilder::InsertionGuard guard(rewriter);
-        rewriter.setInsertionPoint(matmulOp);
+        rewriter.setInsertionPoint(genericOp);
         auto tilingResult = scf::tileUsingSCF(
-            rewriter, cast<TilingInterface>(matmulOp.getOperation()),
+            rewriter, cast<TilingInterface>(genericOp.getOperation()),
             tileOption);
         if (failed(tilingResult))
           return signalPassFailure();
-        rewriter.replaceOp(matmulOp, tilingResult->replacements);
-        matmulOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOps.back());
+        rewriter.replaceOp(genericOp, tilingResult->replacements);
+        genericOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOps.back());
       }
 
       // 3.4 Tile innermost loop
       {
         scf::SCFTilingOptions tileOption;
         SmallVector<OpFoldResult> TileSizes(
-            matmulOp.getNumLoops(),
+            genericOp.getNumLoops(),
             getAsIndexOpFoldResult(rewriter.getContext(), 0));
         TileSizes[0] = getAsIndexOpFoldResult(rewriter.getContext(), 1);
         TileSizes[1] = getAsIndexOpFoldResult(rewriter.getContext(), 1);
         tileOption.setTileSizes(TileSizes);
 
-        SmallVector<int64_t> interchange(matmulOp.getNumLoops(), 0);
-        for (auto i = 0UL; i < matmulOp.getNumLoops(); i++) {
+        SmallVector<int64_t> interchange(genericOp.getNumLoops(), 0);
+        for (auto i = 0UL; i < genericOp.getNumLoops(); i++) {
           interchange[i] = i;
         }
         tileOption.setInterchange(interchange);
 
         OpBuilder::InsertionGuard guard(rewriter);
-        rewriter.setInsertionPoint(matmulOp);
+        rewriter.setInsertionPoint(genericOp);
         auto tilingResult = scf::tileUsingSCF(
-            rewriter, cast<TilingInterface>(matmulOp.getOperation()),
+            rewriter, cast<TilingInterface>(genericOp.getOperation()),
             tileOption);
         if (failed(tilingResult))
           return signalPassFailure();
-        rewriter.replaceOp(matmulOp, tilingResult->replacements);
-        matmulOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOps.back());
+        rewriter.replaceOp(genericOp, tilingResult->replacements);
+        genericOp = dyn_cast<linalg::GenericOp>(tilingResult->tiledOps.back());
       }
-
-      // Example for build a for loop
-      // {
-      //     {
-      //         OpBuilder::InsertionGuard guard(rewriter);
-      //         rewriter.setInsertionPointAfter(matmulOp);
-      //         _EasyBuilderBegin_(rewriter, matmulOp.getLoc());
-      //         scf::ForOp loop1;
-      //         _SCFFor_(loop1, 0, MThreads, 1,
-      //         ValueRange{matmulOp.getResult(0)})
-      //         {
-      //             auto pm = loop1.getInductionVar();
-      //             Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
-      //             auto cmpI = _BuildOp_(arith::CmpIOp,
-      //             arith::CmpIPredicate::slt, lbCst, pm); scf::IfOp ifHandle;
-      //             _SCFIf_(ifHandle, cmpI, matmulOp.getResult(0).getType())
-      //             {
-      //                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 2);
-      //                 _SCFYield_(matmulOp.getResult(0));
-      //             }
-      //             _SCFElse_
-      //             {
-      //                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
-      //                 _SCFYield_(matmulOp.getResult(0));
-      //             }
-      //             _SCFYield_(ifHandle.getResult(0));
-      //             loop1.dump();
-      //         }
-
-      //         scf::ForallOp newLoop;
-      //         _SCFForall_(newLoop, SmallVector<int64_t>{0, 0, 0},
-      //         SmallVector<int64_t>{MThreads, NThreads, KThreads},
-      //         SmallVector<int64_t>{1, 1, 1}, std::nullopt, std::nullopt)
-      //         {
-      //             [[maybe_unused]] Value lbCst =
-      //             _BuildOp_(arith::ConstantIndexOp, 1);
-      //             _SCFInParallel_(newLoop){
-      //                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
-      //             }
-      //         }
-      //         newLoop.dump();
-      //         _EasyBuilderEnd_;
-      //     }
-      //     matmulOp.getOperation()->getParentOfType<ModuleOp>().dump();
-      //     exit(0);
-      // }
-
-      /*
-      // Example: register external function
-      {
-        auto mod = matmulOp.getOperation()->getParentOfType<ModuleOp>();
-        ParserConfig cfg(&ctx, true, nullptr);
-        parseSourceString(
-"func.func @matmul2() -> tensor<256x512xf32> {\
-%3 = tensor.empty() : tensor<256x512xf32>\
-return %3 : tensor<256x512xf32>\
-}",
-            &(mod.getOperation()->getRegion(0).getBlocks().front()), cfg);
-        FlatSymbolRefAttr fnName =
-            SymbolRefAttr::get(rewriter.getContext(), "matmul2");
-        auto loc = matmulOp.getOperation()->getLoc();
-        auto libFnType = parseType("() -> tensor<256x512xf32>", &ctx);
-        auto oprand = SmallVector<Value>();
-        func::CallOp call = rewriter.create<func::CallOp>(
-            loc, fnName.getValue(), TypeRange(), oprand);
-        call.dump();
-        exit(0);
-      }*/
-      auto mod = matmulOp.getOperation()->getParentOfType<ModuleOp>();
-      mod.dump();
     });
 
     {
@@ -341,3 +301,71 @@ return %3 : tensor<256x512xf32>\
 };
 
 } // end namespace
+
+// Example for build a for loop
+// {
+//     {
+//         OpBuilder::InsertionGuard guard(rewriter);
+//         rewriter.setInsertionPointAfter(matmulOp);
+//         _EasyBuilderBegin_(rewriter, matmulOp.getLoc());
+//         scf::ForOp loop1;
+//         _SCFFor_(loop1, 0, MThreads, 1,
+//         ValueRange{matmulOp.getResult(0)})
+//         {
+//             auto pm = loop1.getInductionVar();
+//             Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
+//             auto cmpI = _BuildOp_(arith::CmpIOp,
+//             arith::CmpIPredicate::slt, lbCst, pm); scf::IfOp ifHandle;
+//             _SCFIf_(ifHandle, cmpI, matmulOp.getResult(0).getType())
+//             {
+//                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 2);
+//                 _SCFYield_(matmulOp.getResult(0));
+//             }
+//             _SCFElse_
+//             {
+//                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
+//                 _SCFYield_(matmulOp.getResult(0));
+//             }
+//             _SCFYield_(ifHandle.getResult(0));
+//             loop1.dump();
+//         }
+
+//         scf::ForallOp newLoop;
+//         _SCFForall_(newLoop, SmallVector<int64_t>{0, 0, 0},
+//         SmallVector<int64_t>{MThreads, NThreads, KThreads},
+//         SmallVector<int64_t>{1, 1, 1}, std::nullopt, std::nullopt)
+//         {
+//             [[maybe_unused]] Value lbCst =
+//             _BuildOp_(arith::ConstantIndexOp, 1);
+//             _SCFInParallel_(newLoop){
+//                 Value lbCst = _BuildOp_(arith::ConstantIndexOp, 1);
+//             }
+//         }
+//         newLoop.dump();
+//         _EasyBuilderEnd_;
+//     }
+//     matmulOp.getOperation()->getParentOfType<ModuleOp>().dump();
+//     exit(0);
+// }
+
+/*
+// Example: register external function
+{
+  auto mod = matmulOp.getOperation()->getParentOfType<ModuleOp>();
+  ParserConfig cfg(&ctx, true, nullptr);
+  parseSourceString(
+"func.func @matmul2() -> tensor<256x512xf32> {\
+%3 = tensor.empty() : tensor<256x512xf32>\
+return %3 : tensor<256x512xf32>\
+}",
+      &(mod.getOperation()->getRegion(0).getBlocks().front()), cfg);
+  FlatSymbolRefAttr fnName =
+      SymbolRefAttr::get(rewriter.getContext(), "matmul2");
+  auto loc = matmulOp.getOperation()->getLoc();
+  auto libFnType = parseType("() -> tensor<256x512xf32>", &ctx);
+  auto oprand = SmallVector<Value>();
+  func::CallOp call = rewriter.create<func::CallOp>(
+      loc, fnName.getValue(), TypeRange(), oprand);
+  call.dump();
+  exit(0);
+}*/
